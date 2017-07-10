a7df551bf3502b64e222b909b6ded68b421a04dc
unknown <>
2017-07-10 14:44:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lihan/day_01_oschina/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.lihan.day_01_oschina;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+
+    }
+
+    private void initView() {
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+
+                break;
+        }
+    }
+}
